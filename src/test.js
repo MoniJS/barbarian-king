5e18df374a983e4431ac059c961b707a9f1f60bc
Moni.S <40552237+MoniS10@users.noreply.github.com>
2019-07-20 23:09:31 -0400

    fixed eslint problems

--- src/test.js
+++ src/test.js
@@ -1,9 +1,9 @@
 const isReachable = require('is-reachable');
 
 (async () => {
-    console.log(await isReachable('developer.clashofclans.com'));
-    //=> true
+	console.log(await isReachable('developer.clashofclans.com'));
+	// => true
 
-    console.log(await isReachable('google.com'));
-    //=> true
+	console.log(await isReachable('google.com'));
+	// => true
 })();
